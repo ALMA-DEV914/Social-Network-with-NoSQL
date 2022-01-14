2fb8e586d87b899e57236f2668fa46220f47458e
ALMA_DEV914 <aungonalna58@gamil.com>
2022-01-13 21:44:45 -0800

    add thoughts controller

--- models/Thoughts.js
+++ models/Thoughts.js
@@ -1,6 +1,7 @@
 const { Schema, model } = require('mongoose');
 const reactionSchema = require('./Reaction');
 const dateFormat = require('../utils/dateFormat');
+
 const thoughtSchema = new Schema(
   {
     thoughtText: {
@@ -32,6 +33,6 @@ thoughtSchema.virtual('reactionCount').get(function() {
   return this.reactions.length;
 });
 
-const Thought = model('Thought', thoughtSchema);
+const Thought = model('Thoughts', thoughtSchema);
 module.exports = Thought;
 
