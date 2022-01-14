1fc210fc5bef90be4d7bfbe8d56cc597cf7e7439
ALMA_DEV914 <aungonalna58@gamil.com>
2022-01-13 21:48:59 -0800

    add thoughts routes

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,8 +1,10 @@
 const router = require("express").Router();
 
 const userRoutes = require("./user-routes");
+const thougthsRoutes = require("./thoughts-routes");
 
 router.use("/users", userRoutes);
+router.use("/thoughts", thougthsRoutes);
 
 module.exports = router;
 
